4b4bff9388c3c28e2c2e5f9da18c00ba6e3e8965
ErikCH <icystorm@gmail.com>
2011-03-26 10:37:09 -0700

    first commit

---
+++ BouncingBalls/BouncingBallMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BouncingBalls
+{
+    public partial class BouncingBallMain : Form
+    {
+        public BouncingBallMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
